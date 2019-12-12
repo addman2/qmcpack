9cd0bc39d02172b1bb36291199ac1683669b2092
Otto <pravod@gmail.com>
2019-12-12 04:53:50 +0100

    Minor changes so doxygen would compile without an error

--- src/QMCWaveFunctions/WaveFunctionComponent.h
+++ src/QMCWaveFunctions/WaveFunctionComponent.h
@@ -570,7 +570,7 @@ struct WaveFunctionComponent : public QMCTraits
     }
   }
 
-  /** Calculates the derivatives of \f$ \grad(\textrm{log}(\psif)) \f$ with respect to
+  /** Calculates the derivatives of \f$ \grad(\textrm{log}(\psif)) $\f with respect to
       the optimizable parameters, and the dot product of this is then
       performed with the passed-in G_in gradient vector. This object is then
       returned as dgradlogpsi.
